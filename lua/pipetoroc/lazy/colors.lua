03337a2c748672a3850bc9ac7a02026bf5a07e16
Felipe Toro C <ftorocastilla@gmail.com>
2024-05-18 14:34:28 -0300

    feat: include lsp-config and atocomplete

--- lua/pipetoroc/lazy/colors.lua
+++ lua/pipetoroc/lazy/colors.lua
@@ -1,10 +1,9 @@
 function ColorMyPencils(color)
-	color = color or "rose-pine"
-	vim.cmd.colorscheme(color)
+    color = color or "rose-pine"
+    vim.cmd("colorscheme " .. color)
 
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 return {
@@ -27,8 +26,6 @@ return {
                     floats = "dark", -- style for floating windows
                 },
             })
-
-            ColorMyPencils("tokyonight")
         end
     },
 
@@ -43,10 +40,18 @@ return {
                 },
             })
 
-            vim.cmd("colorscheme rose-pine")
-
         end
     },
+    -- Lazy
+    {
+        "olimorris/onedarkpro.nvim",
+        config = function()
+            require("onedarkpro").setup({
+                
+            })
 
+            ColorMyPencils("onedark_vivid")
+        end
+    },
 
 }
--- lua/pipetoroc/lazy/tresitter.lua
+++ lua/pipetoroc/lazy/tresitter.lua
@@ -6,7 +6,7 @@ return {
             -- A list of parser names, or "all"
             ensure_installed = {
                 "vimdoc", "javascript", "typescript", "c", "lua", "rust",
-                "jsdoc", "bash","java",
+                "jsdoc", "bash", "java",
             },
 
             -- Install parsers synchronously (only applied to `ensure_installed`)
